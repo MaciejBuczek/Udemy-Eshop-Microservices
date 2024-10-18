a05fd3bbc420fd8431a745cb89b7603cbc9e94bf
Maciej Buczek <maciejbuczek98@gmail.com>
2024-10-18 11:18:37 +0200

    Common microservice created. Implemented CQRS MediatR generic interfaces

---
+++ src/Common/Common/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Common.CQRS
+{
+
+    public interface IComman : ICommand<Unit>
+    {
+    }
+
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
