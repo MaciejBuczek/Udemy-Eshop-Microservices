0003cf76e14d773a09230b410eb09dd57173dc6c
Maciej Buczek <maciejbuczek98@gmail.com>
2024-10-23 22:26:22 +0200

    Registered services in Program.cs

---
+++ src/Common/Common/Carter/StaticDependencyContextCatalog.cs
@@ -0,0 +1,15 @@
+﻿using Carter;
+using System.Reflection;
+
+namespace Common.Carter
+{
+    public class StaticDependencyContextCatalog(Assembly assembly) : DependencyContextAssemblyCatalog
+    {
+        private readonly Assembly _assembly = assembly;
+
+        public override IReadOnlyCollection<Assembly> GetAssemblies()
+        {
+            return [_assembly];
+        }
+    }
+}
--- src/catalog/Catalog.API/Program.cs
+++ src/catalog/Catalog.API/Program.cs
@@ -1,4 +1,15 @@
+using Common.Carter;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter(new StaticDependencyContextCatalog(typeof(Program).Assembly));
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
