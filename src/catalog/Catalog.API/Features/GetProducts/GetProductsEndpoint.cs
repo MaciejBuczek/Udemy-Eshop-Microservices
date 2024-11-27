e67d94799188d15cc6cd7608adbb173128fc7d81
Maciej Buczek <maciejbuczek98@gmail.com>
2024-11-27 14:37:22 +0100

    Implemented GetProducts and GetProductById endpoints and handlers

---
+++ src/catalog/Catalog.API/Features/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,41 @@
+﻿
+using Catalog.API.Features.CreateProduct;
+
+namespace Catalog.API.Features.GetProducts
+{
+    public record GetProductsResponse(IEnumerable<Product> Products);
+
+    public record GetProductResponse(Product Product);
+
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+
+                var response = result.Adapt<GetProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product")
+            .WithDescription("Get Product By Id");
+        }
+    }
+}
---
+++ src/catalog/Catalog.API/Features/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace Catalog.API.Features.GetProducts
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductResult>;
+    public record GetProductResult(Product Product);
+
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>, IQueryHandler<GetProductByIdQuery, GetProductResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("{@Name} called with {@Query}", nameof(GetProductsQueryHandler), query);
+            
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            
+            return new GetProductsResult(products);
+        }
+
+        public async Task<GetProductResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("{@Name} called with {@Query}", nameof(GetProductResult), query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if(product != null)
+            {
+                return new GetProductResult(product);
+            }
+
+            throw new ProductNotFoundException();
+        }
+    }
+}
