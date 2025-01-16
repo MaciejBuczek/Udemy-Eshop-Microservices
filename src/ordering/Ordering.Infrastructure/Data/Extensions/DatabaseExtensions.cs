75585b06bd28a9b73e6d7642e9349e08717fc5bc
Maciej Buczek <maciejbuczek98@gmail.com>
2025-01-16 21:22:04 +0100

    Added db initialization on dev app startup

---
+++ src/ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Ordering.Infrastructure.Data.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static void InitializeDatabase(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            context.Database.MigrateAsync().GetAwaiter().GetResult();
+        }
+    }
+}
