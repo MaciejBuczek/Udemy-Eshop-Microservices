bee200158cac6245402d30d16823e4365360a5b9
Maciej Buczek <maciejbuczek98@gmail.com>
2025-01-17 12:32:36 +0100

    Create order command classes and records implemented

--- src/ordering/Ordering.Application/GlobalUsing.cs
+++ src/ordering/Ordering.Application/GlobalUsing.cs
@@ -1,1 +1,4 @@
 ﻿global using Ordering.Domain.Enums;
+global using Common.CQRS;
+global using Ordering.Application.DTOs;
+global using FluentValidation;
---
+++ src/ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public record CreateOrderResult(Guid Id);
+    public record CreateOrderCommand(OrderDTO Order) : ICommand<CreateOrderResult>;
+
+    public class CreateOrderCommandValidetor : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidetor()
+        {
+            RuleFor(x => x.Order.OrderName).NotEmpty();
+            RuleFor(x => x.Order.CustomerId).NotEqual(Guid.Empty);
+            RuleFor(x => x.Order.OrderItems).NotEmpty();
+        }
+    }
+}
