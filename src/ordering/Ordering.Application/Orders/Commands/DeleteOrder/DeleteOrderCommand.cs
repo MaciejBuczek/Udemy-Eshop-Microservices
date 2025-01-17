a007aeb6b6225769a35ee496fa3863e760df3884
Maciej Buczek <maciejbuczek98@gmail.com>
2025-01-17 13:28:23 +0100

    Implemented delete order command and handler

---
+++ src/ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public record DeleteOrderCommandResult(bool IsSucceded);
+    public record DeleteOrderCommand(Guid OrderId) : ICommand<DeleteOrderCommandResult>;
+
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(x => x.OrderId).NotEmpty();
+        }
+    }
+}
--- src/ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
+++ src/ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -6,7 +6,7 @@
         {
             var orderId = OrderId.Of(command.Order.Id);
             var order = await dbContext.Orders.FindAsync(orderId, cancellationToken) ?? 
-                throw new OrderNotFoundException(command.Order.Id);
+                throw new OrderNotFoundException(command.Order.Id.ToString());
             
             dbContext.Orders.Update(order);
             await dbContext.SaveChangesAsync(cancellationToken);
