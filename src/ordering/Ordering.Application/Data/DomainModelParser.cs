afde243dc9750b908e56329a5897a8d6a8f8a5ce
Maciej Buczek <maciejbuczek98@gmail.com>
2025-01-21 10:25:02 +0100

    Get orders by name query and handler implemented

--- src/ordering/Ordering.Application/Data/DomainModelParser.cs
+++ src/ordering/Ordering.Application/Data/DomainModelParser.cs
@@ -23,5 +23,49 @@
                 paymentDTO.Cvv,
                 paymentDTO.PaymentMethod);
         }
+
+        public static OrderDTO ParseOrderDTO(Order order)
+        {
+            return new OrderDTO(
+                order.Id.Id,
+                order.CustomerId.Id,
+                order.OrderName.Name,
+                ParseAddressDTO(order.ShippingAddress),
+                ParseAddressDTO(order.BillingAddress),
+                ParsePaymentDTO(order.Payment),
+                order.Status,
+                order.OrderItems.Select(oi => ParseOrderItemDTO(oi)).ToList());
+        }
+
+        public static AddressDTO ParseAddressDTO(Address address)
+        {
+            return new AddressDTO(
+                address.FirstName,
+                address.LastName,
+                address.EmailAddress,
+                address.AddressLine,
+                address.Country,
+                address.State,
+                address.ZipCode);
+        }
+
+        public static PaymentDTO ParsePaymentDTO(Payment payment)
+        {
+            return new PaymentDTO(
+                payment.CardName,
+                payment.CardNumber,
+                payment.Expiration,
+                payment.CVV,
+                payment.PaymentMethod);
+        }
+
+        public static OrderItemDTO ParseOrderItemDTO(OrderItem orderItem)
+        {
+            return new OrderItemDTO(
+                orderItem.OrderId.Id,
+                orderItem.ProductId.Id,
+                orderItem.Quantity,
+                orderItem.Price);
+        }
     }
 }
---
+++ src/ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName
+{
+    public class GetOrdersByNameQueryHandler(IAppDbContext dbContext) : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameQueryResult>
+    {
+        public async Task<GetOrdersByNameQueryResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking()
+                .Where(o => o.OrderName.Name.Contains(query.Name))
+                .OrderBy(o => o.OrderName)
+                .ToListAsync(cancellationToken);
+
+            var orderDTOs = orders.Select(o => DomainModelParser.ParseOrderDTO(o));
+
+            return new GetOrdersByNameQueryResult(orderDTOs);
+        }
+    }
+}
