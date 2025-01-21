a2bfd7d3a8b042aa7fd34befbc3b38a1579313fc
Maciej Buczek <maciejbuczek98@gmail.com>
2025-01-21 10:38:11 +0100

    Get orders by customer query and handler implemented

---
+++ src/ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public record GetOrdersByCustomerQueryResult(IEnumerable<OrderDTO> Orders);
+    public record GetOrdersByCustomerQuery(Guid CustomerId) : IQuery<GetOrdersByCustomerQueryResult>;
+
+    public class GetORdersByCustomerValidator : AbstractValidator<GetOrdersByCustomerQuery>
+    {
+        public GetORdersByCustomerValidator()
+        {
+            RuleFor(x => x.CustomerId).NotEmpty();
+        }
+    }
+}
---
+++ src/ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public class GetOrdersByCustomerQueryHandler(IAppDbContext dbContext) : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerQueryResult>
+    {
+        public async Task<GetOrdersByCustomerQueryResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking()
+                .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+                .OrderBy(o => o.OrderName.Name)
+                .ToListAsync(cancellationToken);
+
+            var orderDTOs = orders.Select(o => DomainModelParser.ParseOrderDTO(o));
+
+            return new GetOrdersByCustomerQueryResult(orderDTOs);
+        }
+    }
+}
