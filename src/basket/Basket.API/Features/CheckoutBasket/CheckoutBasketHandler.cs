bd83872cabef7d00d5f2e968e0a5c6c2db0e13f3
Maciej Buczek <maciejbuczek98@gmail.com>
2025-01-27 15:28:09 +0100

    Implemented checkout basket endpoint and handler

---
+++ src/basket/Basket.API/Features/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace Basket.API.Features.CheckoutBasket
+{
+    public record CheckoutBasketResult(bool IsSuccess);
+    public record CheckoutBasketCommand(BasketCheckoutDTO BasketCheckout) : ICommand<CheckoutBasketResult>;
+    internal class CheckoutBasketHandler(IBasketRepository repository, IPublishEndpoint publishEndpoint)
+        : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+    {
+        public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+        {
+            var basket = await repository.GetBasket(command.BasketCheckout.Username, cancellationToken);
+            if (basket is null)
+                return new CheckoutBasketResult(false);
+
+            var eventMessage = command.BasketCheckout.Adapt<BasketCheckoutEvent>();
+            eventMessage.TotalPrice = basket.TotalPrice;
+
+            await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+            await repository.DeleteBasket(command.BasketCheckout.Username, cancellationToken);
+
+            return new CheckoutBasketResult(true);
+        }
+    }
+
+    public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCommand>
+    {
+        public CheckoutBasketCommandValidator()
+        {
+            RuleFor(x => x.BasketCheckout).NotNull();
+            RuleFor(x => x.BasketCheckout.Username).NotEmpty();
+        }
+    }
+}
