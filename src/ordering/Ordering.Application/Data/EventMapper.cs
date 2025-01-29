86c7c724dc9a9ac02ab1ea0f0d4b7ce51911d155
Maciej Buczek <maciejbuczek98@gmail.com>
2025-01-29 11:57:50 +0100

    Implemented domain OrderCreatedEventHandler

---
+++ src/ordering/Ordering.Application/Data/EventMapper.cs
@@ -0,0 +1,30 @@
+﻿namespace Ordering.Application.Data
+{
+    public static class EventMapper
+    {
+        public static CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutEvent checkoutEvent)
+        {
+            var addressDTO = new AddressDTO(checkoutEvent.FirstName, checkoutEvent.LastName, checkoutEvent.EmailAddress, checkoutEvent.AddressLine, checkoutEvent.Country, checkoutEvent.State, checkoutEvent.ZipCode);
+            var paymentDTO = new PaymentDTO(checkoutEvent.CardName, checkoutEvent.CardNumber, checkoutEvent.Expiration, checkoutEvent.CVV, checkoutEvent.PaymentMethod);
+            var orderId = Guid.NewGuid();
+
+            var orderDTO = new OrderDTO(
+                Id: orderId,
+                CustomerId: checkoutEvent.CustomerId,
+                OrderName: checkoutEvent.Username,
+                ShippingAddress: addressDTO,
+                BillingAddress: addressDTO,
+                Payment: paymentDTO,
+                Status: OrderStatus.Pending,
+                OrderItems:
+                [
+                    new OrderItemDTO(orderId, new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61"),2, 500),
+                    new OrderItemDTO(orderId, new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914"), 1, 400)
+                ]);
+
+            return new CreateOrderCommand(orderDTO);
+        }
+
+        public Order
+    }
+}
--- src/ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,12 +1,14 @@
 ﻿namespace Ordering.Application.Orders.EventHandlers.Domain
 {
-    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger, IPublishEndpoint publishEndpoint)
         : INotificationHandler<OrderCreatedEvent>
     {
-        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
         {
-            logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType());
-            return Task.CompletedTask;
+            logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType());
+
+            var orderCreatedIntegrationEvent = DomainModelParser.ParseOrderDTO(domainEvent.Order);
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
         }
     }
 }
