cc15a27db07b653d10dce37ea340df01a520c4d1
Maciej Buczek <maciejbuczek98@gmail.com>
2025-01-30 12:47:36 +0100

    Fixed spelling error in checkout basket url

--- src/basket/Basket.API/Features/CheckoutBasket/CheckoutBasketEndpoint.cs
+++ src/basket/Basket.API/Features/CheckoutBasket/CheckoutBasketEndpoint.cs
@@ -7,7 +7,7 @@
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapPost("basket/checkout", async (CheckoutBasketRequest request, ISender sender) =>
+            app.MapPost("/basket/checkout", async (CheckoutBasketRequest request, ISender sender) =>
             {
                 var command = request.Adapt<CheckoutBasketCommand>();
                 var result = await sender.Send(command);
