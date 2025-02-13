9892c3376e437ca79158f3752ecf99586f296fad
Maciej Buczek <maciejbuczek98@gmail.com>
2025-02-13 10:18:53 +0100

    Created basket model, requests and responses

---
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCartItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Shopping.Web.Models.Basket
+{
+    public class ShoppingCartItemModel
+    {
+        public string Name { get; set; } = default!;
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string Color { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
